87a6270ced1bb43bf9f95b6eb1f8594e18f4a9f0
Zaicev Vladimir <zaicevvv1991@gmail.com>
2019-10-06 14:14:15 +0300

    authPage

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import css from './AuthPage.module.css';
 
 const Login = () => (
-  <>
-    <h1>Login</h1>
-    <Link to="/register">register</Link>
-  </>
+  <div className={css.container}>
+    <h2 className={css.h2}>Виконуй завдання - отримай класні призи!</h2>
+    <button type="button" className={css.btn}>
+      Вхід
+    </button>
+    <Link to="/register" className={css.regText}>
+      Реєстрація
+    </Link>
+  </div>
 );
 
 const AuthPage = () => <Login />;
