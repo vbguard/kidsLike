2addfdd022f0237361cbaeabbbbd1e8298a517dc
Viktor Bulvarenko <silken.ua@gmail.com>
2019-10-15 20:57:14 +0300

    hdfhfhdfjd

--- src/components/DaySelect/DayOne.js
+++ src/components/DaySelect/DayOne.js
@@ -1,28 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import styles from './DayOne.module.css';
+import checkedIcon from '../../assets/icons/check_box-24px.svg';
+import boxIcon from '../../assets/icons/check_box_outline_blank-24px.svg';
 
-const stylesSelectDay = [styles.selectday];
 const label = [styles.label];
 const checkboxs = [styles.checkboxs];
-const fake = [styles.fake];
+
 const days = [styles.days];
 
-const DayOne = ({ day }) => (
-  <div className={stylesSelectDay}>
-    <label className={label}>
-      <input type="checkbox" className={checkboxs} id="sw1" />
-      <span className={fake}></span>
-      <span className={days}>{day}</span>
-    </label>
-  </div>
+const Chekbox = styled.span`
+  &::before {
+    content: '';
+    position: absolute;
+    background-image: ${props => (props.checked ? `url(${checkedIcon})` : `url(${boxIcon})`)};
+    width: 24px;
+    height: 24px;
+    transition: 0.4s;
+  }
+`;
+
+const DayOne = ({ day, checked, onChange }) => (
+  <label className={label}>
+    <input
+      type="checkbox"
+      className={checkboxs}
+      checked={checked}
+      id="sw1"
+      onChange={e => {
+        e.preventDefault();
+        console.log(e.target.checked);
+        onChange(state => ({ ...state, mon: e.target.checked }));
+      }}
+    />
+    <Chekbox checked={checked} />
+    <span className={days}>{day}</span>
+  </label>
 );
-
-DayOne.propTypes = {
-  day: PropTypes.string
-};
-
-DayOne.defaultProps = {
-  day: 'Пн'
-};
+DayOne.propTypes = { day: PropTypes.string, checked: PropTypes.bool, onChange: PropTypes.func };
+DayOne.defaultProps = { day: 'Пн' };
 export default DayOne;
--- src/components/DaySelect/DaySelect.js
+++ src/components/DaySelect/DaySelect.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
 import styles from './DaySelect.module.css';
 // import Icon from '../Icon/Icon';
@@ -6,16 +6,23 @@ import DayOne from './DayOne';
 
 const stylesDay = [styles.week];
 
-const DaySelect = () => (
-  <div className={stylesDay}>
-    <DayOne />
-    <DayOne day="Вт" />
-    <DayOne day="Ср" />
-    <DayOne day="Чт" />
-    <DayOne day="Пт" />
-    <DayOne day="Сб" />
-    <DayOne day="Нд" />
-  </div>
-);
+const DaySelect = () => {
+  const [daySelected, setDaySelected] = useState({
+    mon: false,
+    thu: false
+  });
+
+  return (
+    <div className={stylesDay}>
+      <DayOne checked={daySelected.mon} onChange={setDaySelected} />
+      <DayOne day="Вт" checked={daySelected.thu} onChange={setDaySelected} />
+      <DayOne day="Ср" />
+      <DayOne day="Чт" />
+      <DayOne day="Пт" />
+      <DayOne day="Сб" />
+      <DayOne day="Нд" />
+    </div>
+  );
+};
 
 export default DaySelect;
