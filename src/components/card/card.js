f0cc417bb0967062acb952085d1cd0f8233dd6fc
Konstantin <costaslavich@gmail.com>
2019-10-17 00:03:28 +0300

    card styles finish

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Card.module.css';
+import pic6 from '../../assets/images/tasksImage/pic-6.png';
 
-const Card = ({ image = '', title = '' }) => (
+const Card = ({ title = '' }) => (
   <div className={styles.Card}>
     <div className={styles.pic}>
-      <img src={image} alt="img" />
+      <img
+        // srcset="pic-6.png 320w,
+        // pic-6@2x.png 768w,
+        // pic-6@3x.pgn 1280w"
+        src={pic6} alt="img" />
     </div>
-    <h2 className={styles.title}>{title}</h2>
+    <div className={styles.pic_footer}>
+      <h2 className={styles.title}>{title}</h2>
+    </div>
   </div>
 );
 
 Card.defaultProps = {
-  image: '../assets/images/tasksImage/pic-6@2x.png',
   title: 'Прибрати у шафi'
 };
 
 Card.propTypes = {
-  image: PropTypes.string,
   title: PropTypes.string
 };
 
