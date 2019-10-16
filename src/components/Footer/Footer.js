31c0800973d12126b619584feddbd082d33286f5
Zaicev Vladimir <zaicevvv1991@gmail.com>
2019-10-16 11:06:57 +0300

    more fixes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,22 +3,18 @@ import { Link } from 'react-router-dom';
 import css from './Footer.module.css';
 import Icon from '../Icon/Icon';
 
-const Footer = () => {
-  // const {screenWidth} = window;
-  const a = 1;
-  return (
-    <footer className={css.footer}>
-      <Link to="/" className={css.KidsLike}>
-        KidsLike <Icon icon="Logo" className={css.icon} />
-      </Link>
-      <span className={(css.text, css.borders)}>
-        Робимо життя батьків і дітей ізі{' '}
-        <span role="img" aria-label="smile">
-          😄
-        </span>
+const Footer = () => (
+  <footer className={css.footer}>
+    <Link to="/" className={css.KidsLike}>
+      KidsLike <Icon icon="Logo" className={css.icon} />
+    </Link>
+    <span className={(css.text, css.borders)}>
+      Робимо життя батьків і дітей ізі{' '}
+      <span role="img" aria-label="smile">
+        😄
       </span>
-      <span className={css.text}>2019{a}</span>
-    </footer>
-  );
-};
+    </span>
+    <span className={css.text}>2019</span>
+  </footer>
+);
 export default Footer;
