06da0f9758feee898e62bc60336ebc8d15b553a2
Zaicev Vladimir <zaicevvv1991@gmail.com>
2019-10-16 11:26:56 +0300

    register

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import AuthPage from './pages/AuthPage/AuthPage';
+import Register from './components/Register/Register';
 // import ProtectedComponent from './hoc/ProtectedComponent';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={AuthPage} />
+      <Route path="/register" component={Register} />
     </Switch>
   );
 }
