46380d27b7f098061f79b7d7f6ae083c0533be90
Viktor Bulvarenko <3513401+vbguard@users.noreply.github.com>
2019-10-18 02:55:42 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
 import AuthPage from './pages/AuthPage/AuthPage';
-import DaySelect from './components/DaySelect/DaySelect';
 import CardTogle from './components/CardTogle/CardTogle';
 import DashboardPage from './pages/DashboardPage/DashboardPage';
 import PlanningPage from './pages/PlanningPage/PlanningPage';
@@ -14,7 +12,6 @@ function App() {
   return (
     <Switch>
       <Route exact path="/" component={AuthPage} />
-      <Route path="/daySelect" component={DaySelect} />
       <Route path="/cardTogle" render={props => <CardTogle {...props} isTogleOn="true" />} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
