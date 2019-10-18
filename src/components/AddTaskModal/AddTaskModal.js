60ac49ab616f0c4606c24670f0915ec4495e4e34
Elena Frolova <elenschip777@gmail.com>
2019-10-18 22:19:46 +0300

    fixed errors

--- src/components/AddTaskModal/AddTaskModal.js
+++ src/components/AddTaskModal/AddTaskModal.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import Icon from '../Icon/Icon';
 import styles from './AddTaskModal.module.css';
 
-const AddTaskModal = ({ onChange }) => (
+const AddTaskModal = () => (
   <div
     className={styles.backdrop}
-    onClick={e => {
-      e.preventDefault();
-      onChange();
-    }}
-    onKeyDown={() => {}}
-    role="button"
-    tabIndex="0"
+    // onClick={e => {
+    //   e.preventDefault();
+    //   onChange();
+    // }}
+    // onKeyDown={() => {}}
+    // role="button"
+    // tabIndex="0"
   >
     <div className={styles.modal}>
       <div className={styles.imgConteiner}>
@@ -20,10 +20,10 @@ const AddTaskModal = ({ onChange }) => (
       </div>
       <form
         className={styles.form}
-        onSubmit={e => {
-          e.preventDefault();
-          onChange();
-        }}
+        // onSubmit={e => {
+        //   e.preventDefault();
+        //   onChange();
+        // }}
       >
         <label htmlFor="taskName">
           <input type="text" id="taskName" className={styles.input} placeholder="Додай собі завдання" />
@@ -34,8 +34,8 @@ const AddTaskModal = ({ onChange }) => (
   </div>
 );
 
-AddTaskModal.propTypes = {
-  onChange: PropTypes.func
-};
+// AddTaskModal.propTypes = {
+//   onChange: PropTypes.func
+// };
 
 export default AddTaskModal;
