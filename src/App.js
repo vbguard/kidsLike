cbcfa67add24c41601bbdb5300e266ce42d6f0c5
Konstantin <costaslavich@gmail.com>
2019-10-19 16:39:11 +0300

    fixed styles card

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import PlanningPage from './pages/PlanningPage/PlanningPage';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
 import Header from './components/Header/Header';
+// import TaskList from './components/TaskList/TaskList';
+import Card from './components/Card/Card';
 import ContactsPage from './pages/ContactsPage/ContactsPage';
 import Footer from './components/Footer/Footer';
+
 // import ProtectedComponent from './hoc/ProtectedComponent';
 
 function App() {
@@ -20,6 +23,8 @@ function App() {
         <Route path="/register" component={Register} />
         <Route path="/dashboard" component={DashboardPage} />
         <Route path="/planning" component={PlanningPage} />
+        {/* <Route path="/tasklist" component={TaskList} /> */}
+        <Route path="/card" component={Card} />
         <Route path="/contacts" component={ContactsPage} />
       </Switch>
       <Footer />
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Card.module.css';
-import pic6 from '../../assets/images/tasksImage/pic-6.png';
+import CardTogle from '../CardTogle/CardTogle';
+// import pic6 from '../../assets/images/tasksImage/pic-6@2x.png';
 
-const Card = ({ title = '' }) => (
+// const small = '300.png';
+// const medium = '768.png';
+// const large = '1280.png';
+
+// const ResponsiveImage = () => (
+//   <img
+//     src={small}
+//     srcSet={`${small} 300w,
+//     ${medium} 768w,
+//     ${large} 1280w`}
+//     alt="img-for-choose"
+//   />
+// );
+
+const Card = ({ title }) => (
   <div className={styles.Card}>
-    <div className={styles.pic}>
-      <img
-        // srcset="pic-6.png 320w,
-        // pic-6@2x.png 768w,
-        // pic-6@3x.pgn 1280w"
-        src={pic6}
-        alt="img"
-      />
-    </div>
-    <div className={styles.pic_footer}>
+    <div className={styles.pic}>{/* <ResponsiveImage picture={small} /> */}</div>
+    <div className={styles.pic_footer_container}>
       <h2 className={styles.title}>{title}</h2>
+      <CardTogle />
     </div>
   </div>
 );
