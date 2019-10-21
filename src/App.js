83dcbd416b9cdd1fc5782804e4c22ef3114e3614
Viktor Bulvarenko <3513401+vbguard@users.noreply.github.com>
2019-10-22 02:26:14 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,26 @@
 import React from 'react';
-
 import { Switch, Route } from 'react-router-dom';
-
 import AuthPage from './pages/AuthPage/AuthPage';
-
 import DashboardPage from './pages/DashboardPage/DashboardPage';
-
 import PlanningPage from './pages/PlanningPage/PlanningPage';
-
 import Register from './components/Register/Register';
-
 import Login from './components/Login/Login';
-
 import Header from './components/Header/Header';
-
 import ContactsPage from './pages/ContactsPage/ContactsPage';
-
 import CardToggle from './components/Card/Card';
-
 // import ProtectedComponent from './hoc/ProtectedComponent';
 
 function App() {
   return (
     <>
       <Header />
-
       <Switch>
         <Route exact path="/" component={AuthPage} />
-
         <Route path="/login" component={Login} />
-
         <Route path="/register" component={Register} />
-
         <Route path="/dashboard" component={DashboardPage} />
-
         <Route path="/planning" component={PlanningPage} />
-
         <Route path="/contacts" component={ContactsPage} />
-
         <Route path="/card" component={CardToggle} />
       </Switch>
     </>
