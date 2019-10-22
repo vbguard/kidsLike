d81e323b7e62fdc03df4ed362d4dc2e09f602ef1
Konstantin <costaslavich@gmail.com>
2019-10-22 14:28:43 +0300

    added TaskId

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Card.module.css';
 import CardTogle from '../CardTogle/CardToggle';
+import DaySelect from '../DaySelect/DaySelect';
 import c from '../../assets/images/tasksImage/pic-3@2x.png';
 
-const Card = ({ cardTitle, imageUrl }) => (
+const Card = ({ cardTitle, imageUrl, isPlanning, taskId }) => (
   <div className={styles.Card_container}>
     <div className={styles.Card_list}>
       <div className={styles.pic}>
@@ -12,7 +13,7 @@ const Card = ({ cardTitle, imageUrl }) => (
       </div>
       <div className={styles.pic_footer}>
         <h2 className={styles.title}>{cardTitle}</h2>
-        <CardTogle />
+        {isPlanning ? <CardTogle taskId={taskId} /> : <DaySelect />}
       </div>
     </div>
   </div>
@@ -20,12 +21,15 @@ const Card = ({ cardTitle, imageUrl }) => (
 
 Card.propTypes = {
   cardTitle: PropTypes.string,
-  imageUrl: PropTypes.string
+  imageUrl: PropTypes.string,
+  isPlanning: PropTypes.bool,
+  taskId: PropTypes.string.isRequired
 };
 
 Card.defaultProps = {
-  title: 'Поприбирати у шафі',
-  imageUrl: { c }
+  cardTitle: 'Прибрати у шафі',
+  imageUrl: { c },
+  isPlanning: 'false'
 };
 
 export default Card;
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Card from '../Card/Card';
+// import Card from '../Card/Card';
 import styles from './TaskList.module.css';
 
 const TaskList = ({ tasks }) => (
