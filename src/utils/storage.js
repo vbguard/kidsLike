48f285bbac616c1c71e01ba7bfa175f3da64066d
Viktor Bulvarenko <3513401+vbguard@users.noreply.github.com>
2019-10-24 19:51:46 +0300

    Update storage.js

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,13 +1,10 @@
-// Тут написати функцію яка буде перевіряти на наявність токену у локал стореджі
-//! І обов'язково повина щось вертати або сам токен або null
-
-//// Необходимо проверить на работоспособность /////
-
-const isToken = () => {
+const getToken = () => {
   const token = localStorage.getItem('token');
 
-  if (token !== null) {
+  if (token) {
     return Json.parse(token);
   }
-  return null;
+  return false;
 };
+
+export default getToken
