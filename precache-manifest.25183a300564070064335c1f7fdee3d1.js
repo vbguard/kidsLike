8e7c2b352a17e9f78b95a67dd0a1423154a6e687
Viktor Bulvarenko <silken.ua@gmail.com>
2019-10-28 20:00:12 +0200

    Updates

---
+++ precache-manifest.25183a300564070064335c1f7fdee3d1.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a5390086217e7436749c687a07be4a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abdad3a1f0aa94d34df0",
+    "url": "/static/css/2.058cbf93.chunk.css"
+  },
+  {
+    "revision": "1ae8f70b6d72c1d7ead1",
+    "url": "/static/css/main.5cccfd58.chunk.css"
+  },
+  {
+    "revision": "abdad3a1f0aa94d34df0",
+    "url": "/static/js/2.8c8513ab.chunk.js"
+  },
+  {
+    "revision": "1ae8f70b6d72c1d7ead1",
+    "url": "/static/js/main.a95f4936.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fea31bf763b9065a35353dbaa002f62a",
+    "url": "/static/media/Elena.fea31bf7.jpg"
+  },
+  {
+    "revision": "45eaba1d916d2ddcf3278fd72b7b25cd",
+    "url": "/static/media/Eugenia.45eaba1d.jpg"
+  },
+  {
+    "revision": "5bbfabce2ddda59e131972640f1d471b",
+    "url": "/static/media/IMG_5858.5bbfabce.JPG"
+  },
+  {
+    "revision": "33d5765a61d081114a1e2ad5b6a5c1da",
+    "url": "/static/media/O.33d5765a.jpg"
+  },
+  {
+    "revision": "cc5eec95e79777dadf9e369fc1872e80",
+    "url": "/static/media/Vladimir.cc5eec95.jpg"
+  },
+  {
+    "revision": "ebe1c3a69ddb3b046edbb1f11f689d78",
+    "url": "/static/media/arrow_drop_down-24px.ebe1c3a6.svg"
+  },
+  {
+    "revision": "0f3e02babe6f9c7f29ecad2fec45a01f",
+    "url": "/static/media/authPageMobile.0f3e02ba.png"
+  },
+  {
+    "revision": "81ae01b155012415c7fe42349f5561fc",
+    "url": "/static/media/bg-auth@2x.81ae01b1.jpg"
+  },
+  {
+    "revision": "ebd94b35e5cace0547aba968b8b45c83",
+    "url": "/static/media/bg-auth@3x.ebd94b35.jpg"
+  },
+  {
+    "revision": "a634cd7e60b89334cf4a3f61940e8d43",
+    "url": "/static/media/burger.a634cd7e.svg"
+  },
+  {
+    "revision": "3a608b84c27c3f09cdffbd3b4cdad411",
+    "url": "/static/media/check_box-24px.3a608b84.svg"
+  },
+  {
+    "revision": "6d816b8d64586442587b5de5ea8b9196",
+    "url": "/static/media/check_box_outline_blank-24px.6d816b8d.svg"
+  },
+  {
+    "revision": "b6d43e8e46d9eeb3cd6cf49071cf330c",
+    "url": "/static/media/ckeckbox.b6d43e8e.png"
+  },
+  {
+    "revision": "a471c5dfd6bedf1fb8e21eb8275daa6c",
+    "url": "/static/media/close.a471c5df.svg"
+  },
+  {
+    "revision": "3b96c4995dd01baed9d57b2ae319f4aa",
+    "url": "/static/media/exit.3b96c499.svg"
+  },
+  {
+    "revision": "80a38ba3048eb72b11e25fd6be5fdb5a",
+    "url": "/static/media/gift-box.80a38ba3.svg"
+  },
+  {
+    "revision": "8bbd445789cac0b2bb682016b9bd429c",
+    "url": "/static/media/logo.8bbd4457.svg"
+  }
+]);
